c579d79cc5c01a9cfa5e6bf4490e597b87f7cacd
Mishthi Jhamb <mishthijhamb637@gmail.com>
2025-02-16 16:26:36 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -17,7 +17,7 @@ int main(){
         if (num2 != 0) {
             printf("%d\n", num1 / num2);
         } else {
-            printf("Error: Division by zero\n");
+            printf("error\n");
         }
     }
     else{
